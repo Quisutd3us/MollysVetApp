00902813331164d294d7e205cd4f4e2293e6b62c
d3us <dnarino@gmail.com>
2023-07-25 10:53:56 -0500

    update header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
       <div className={'flex flex-col align-middle justify-evenly'}>
         <h1 className={'font-black text-5xl text-center'}>{'Molly\'s'} App</h1>
-        <h4 className={'text-indigo-600 text-3xl text-center'}>Vet Control</h4>
+        <h4 className={'text-indigo-600 text-3xl text-center'}>Vet Control @GitHUb</h4>
         <div className="inline-flex items-center justify-center w-full">
           <hr className="w-64 h-1 my-8 bg-gray-200 border-0 rounded dark:bg-gray-700"/>
             <div className="absolute px-4 -translate-x-1/2 bg-gray-100 left-1/2 dark:bg-gray-900">
